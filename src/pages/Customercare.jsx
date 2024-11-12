d5b98dbe9e25efd900bc97fac07bda045f37491c
Azan-Asim <148682737+Azan-Asim@users.noreply.github.com>
2024-11-12 21:08:11 +0500

    updated

--- src/pages/Customercare.jsx
+++ src/pages/Customercare.jsx
@@ -106,8 +106,9 @@ function Customercare() {
     <>
 
       <div className='w-full h-20 bg-[#C3355C] justify-center flex items-center text-xl md:text-4xl font-bold text-white'>Hi. How Can We Help?</div>
-      <div className='bg-slate-100 w-full  pl-5 pr-5 sm:pl-16 sm:pr-16 '><h1 className='text-lg font-semibold pt-8 pb-4 '>Helps & Topics</h1>
-        <div className='bg-white w-full h-80 grid grid-cols-2 sm:grid-cols-3 overflow-auto'>
+      <div className='bg-slate-100 w-full  pl-5 pr-5 sm:pl-16 sm:pr-16 '>
+        {/* <h1 className='text-lg font-semibold pt-8 pb-4 '>Helps & Topics</h1> */}
+        {/* <div className='bg-white w-full h-80 grid grid-cols-2 sm:grid-cols-3 overflow-auto'>
           <div className=' flex-col items-center flex justify-center sm:justify-center  sm:flex sm:items-center gap-4 md:gap-8 '><img className='size-12 md:size-14 ' src="./Shippingdelivery.png" alt="" />
             <h1 className='text-xs sm:text-md lg:text-lg'>Shipping & Delivery</h1>
           </div>
@@ -126,7 +127,7 @@ function Customercare() {
           <div className='  flex-col items-center flex justify-center  sm:justify-center  sm:flex sm:items-center gap-4 md:gap-8'><img className='size-12 md:size-14 ' src="./Accountsetting.png" alt="" />
             <h1 className='text-xs sm:text-md lg:text-lg'>Account Settings</h1>
           </div>
-        </div>
+        </div> */}
         <h1 className=' text-2xl sm:text-4xl font-bold pt-8 pb-4 '>Frequently Asked Questions</h1>
         <Faq data={faq1} />
         <Faq data={faq2} />
